f7a32b0de60dde23cb71d1832f3c44117df810a2
HomeTimes <2300485148@qq.com>
2021-07-04 12:38:53 +0800

    循环队列

---
+++ queue/circleArrayQueue.java
@@ -0,0 +1,115 @@
+package queue;
+
+import java.util.Scanner;
+
+public class circleArrayQueue {
+    int front;
+    //    尾
+    int real;
+    //    首先我们用数组的形式来创建对列，
+//    那么需要声明初始数组的大小；
+    int maxSize;
+    int arr[];
+    //    构造函数：初始化对列
+    public  circleArrayQueue(int maxSize){
+//        初始化
+        front=0;
+        real=0;
+        this.maxSize=maxSize;
+        arr=new int[maxSize];
+    }
+    //    判断队列是否为空
+    public boolean isEmpty(){
+        return  front == real;
+    }
+    //    判断是否为满员了
+    public boolean isFull(){
+        return (real+1)% maxSize== front;
+    }
+    //    开始创建对列的一些功能
+//    添加
+    void addQueue(int yuan){
+        if(isFull()){
+            throw new RuntimeException("队列已满，，，，");
+        }
+        arr[real]=yuan;
+        real = (real+1)% maxSize;
+    }
+    // 出队列
+    public int getQueue(){
+        if(isEmpty()){
+            throw  new  RuntimeException("队列为空。。。。。");
+        }
+       int value= arr[front];
+        front=(front+1)% maxSize;;
+
+        return value;
+    }
+    //    队头的数据
+    int  showFront(){
+        if (isEmpty()) {
+            throw new RuntimeException("队列为空。。。。。");
+        }
+        return  arr[front];
+    }
+    void showQueue(){
+        for (int i=front;i<front+showCount();i++){
+            System.out.printf("arr[%d]=%d\n ",i,arr[i]);
+        }
+    }
+//    有效数ju
+    int   showCount(){
+        if (isEmpty()) {
+            throw new RuntimeException("队列为空。。。。。");
+        }
+        return(real + maxSize -front) % maxSize ;
+    }
+    //    测试
+    public static void main(String[] args) {
+        circleArrayQueue arrayQueue=new circleArrayQueue(3);
+        Scanner scanner=new Scanner(System.in);
+        char key=' ';
+        boolean loop=true;
+        while (loop){
+            System.out.println("add 入队");
+            System.out.println("g   出队");
+            System.out.println("s(show) 获取队列的数据 ");
+            System.out.println("f(front) 获取队头的数据");
+            System.out.println("e(exit system) 推出系统");
+            key= scanner.next().charAt(0);
+            switch (key){
+                case 'a':
+                    try{
+                        System.out.println("添加数据");
+                        int i = scanner.nextInt();
+                        arrayQueue.addQueue(i);
+                    }
+                    catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'g':
+                    try{
+                        int queue = arrayQueue.getQueue();
+                        System.out.println("取出的数据为"+queue);
+                    }
+                    catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'f':
+                    int i1 = arrayQueue.showFront();
+                    System.out.println("front 的数据为"+i1);
+                    break;
+                case 'e':
+                    loop =false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
