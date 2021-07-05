c97efac729053806c8e2191ed2876ea189f4f71c
HomeTimes <2300485148@qq.com>
2021-07-05 22:17:21 +0800

    单向链表 加了更新链表中某个元素的信息

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -7,10 +7,12 @@ public class LinkedList {
           Node  first =new Node(1,"我是第一个node");
           Node  sec =new Node(2,"我是第二个node");
           Node  three =new Node(3,"我是第3个node");
+          
 		LinkededList linkededList =new LinkededList();
-		linkededList.addreal(first);
-		linkededList.addreal(three);
-		linkededList.addSort(sec);
+//		linkededList.addreal(first);
+//		linkededList.addreal(three);
+//		linkededList.addSort(three);
+		linkededList.updateNode(three);
 		linkededList.showLinkedList();
 	}
 
@@ -24,6 +26,7 @@ class LinkededList{
 //	以添加到尾部的方式
 	
 	public void addreal(Node i){
+
 //		想一下是不是要遍历链表才能添加结点到尾部，条件为null
 //		那么遍历是不是要有一个辅助的结点，才能遍历到尾部
 //		我们不能将头结点来遍历，这样会使head结点的位置发生变化
@@ -36,6 +39,7 @@ class LinkededList{
 		}
 	    temp.next=i;
 	}
+//	这是不允许插入同一个元素的情况
      public void addSort(Node i){
     	 Node temp =head;
     	 if(temp.next == null ){
@@ -85,7 +89,36 @@ class LinkededList{
 			System.out.println(temp.toString());
 		}
 	}
-	
+//	更新指定节点信息
+	public void updateNode(Node i){
+		Node temp =head;
+//		判断是否为空，不要先开始就遍历做无用功
+		if(temp.next == null){
+			System.out.println("链表没有元素");
+			return;
+		}
+//		遍历找指定的节点
+//		如果节点存在，遍历
+		boolean flag = false;
+		while(true){
+//			有节点元素，一直找啊找，都找不到
+			if(temp.next ==null){
+                   break;
+			}
+//			找到了
+			if(temp.next.id == i.id){
+				flag = true;
+				break;
+			}
+			temp = temp.next;
+		}
+		 if(flag){
+			 i.next = temp.next.next;
+			 temp.next=i;   		 
+		 }else{
+			 System.out.println("链表中没有找到元素");
+		 }
+	}
 }
 
 //这是链表的节点
@@ -98,11 +131,12 @@ class Node{
 		this.name=name;
 		
 	}
+//	为了显示方便重写toString 方法
 	@Override
 	public String toString() {
 		return "Node [" + " id=" + id + ", name=" + name + "]";
 	}
 	
-//	为了显示方便重写toString 方法
+
 	
 }
